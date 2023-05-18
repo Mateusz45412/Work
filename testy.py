16b58625768ab8727c75dce1bb624dfbd9b3d4a6
Mateusz45412 <mateusz45412@gmail.com>
2023-05-18 22:58:17 +0200

    work in progress

--- testy.py
+++ testy.py
@@ -9,6 +9,7 @@ def cost_project():
     cc_project = int(input(Fore.LIGHTGREEN_EX + "Ile wynosi koszt wykonania projektu ? "))
     return cc_project
 
+
 def func_projects(c_project):
     return print(f"Projekt: {c_project} zł") if c_project != 0 else False
 
