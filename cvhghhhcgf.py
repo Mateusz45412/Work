88b0d4241557c8f9edc1371273dd64dd0eeed71b
Mateusz45412 <mateusz45412@gmail.com>
2023-05-20 23:29:45 +0200

    work in progress

---
+++ cvhghhhcgf.py
@@ -0,0 +1,7 @@
+while True:
+    collision_road_city = input("Czy jest to droga miejska? ")
+    if not collision_road_city in "y" and not collision_road_city in "n":
+        print(f"col {collision_road_city}")
+    elif collision_road_city in "n" :
+        print("comkjhjkiljkhks")
+        break
