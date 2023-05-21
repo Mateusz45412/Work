cc99d46faa1fecf521b92f2f9a5765c2d857f1a5
Mateusz45412 <mateusz45412@gmail.com>
2023-05-21 17:16:56 +0200

    work in progress

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ from colorama import Fore
 line_sn = 5
 meters = 1000
 number_of_trips = 6
-#####
 
-def cost_project():
+
+def func_cost_project():
     cost_project = int(input(Fore.LIGHTGREEN_EX + "Ile wynosi koszt wykonania projektu ? "))
     return cost_project
-
+cost_project = func_cost_project()
 def func_projects(c_project):
     return print(f"Projekt: {c_project} zł") if c_project != 0 else False
 
-func_projects(cost_project())
+func_projects(cost_project)
+suma = cost_project
+wynik = f"suma: {suma}"
+print(wynik)
